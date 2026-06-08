27eb2532a224ee5d565085ac78fd781b124c95ad
AYUSHI KUMARI <162792850+ayushi48@users.noreply.github.com>
2026-06-08 13:34:12 +0530

    Update index.js

--- AlgoLeague-main/AlgoLeague_server/src/index.js
+++ AlgoLeague-main/AlgoLeague_server/src/index.js
@@ -24,7 +24,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://algoleague.vercel.app",
+      "https://algo-league1.vercel.app",
     ],
     credentials: true,
   })
